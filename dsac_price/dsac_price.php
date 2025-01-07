78b872da65b7a1bfe6b6e2afd6e8d4e7783f7132
CTO Ulpik <davichitoc@gmail.com>
2025-01-07 10:50:34 -0500

    fix: in dsac_price plugin

--- dsac_price/dsac_price.php
+++ dsac_price/dsac_price.php
@@ -15,71 +15,68 @@ if (!defined('ABSPATH')) {
 
 // Registrar los scripts y estilos
 function price_product_enqueue_assets() {
-    // Registrar estilos
     wp_register_style('price-dsac-styles', plugin_dir_url(__FILE__) . 'css/price-dsac-styles.css');
-
 }
-
-// Hook para registrar scripts y estilos
 add_action('wp_enqueue_scripts', 'price_product_enqueue_assets');
 
 // Función para renderizar el shortcode
-function price_dsac_shortcode() {
-    function show_price_dsac($atts) {
-        date_default_timezone_set('America/Guayaquil');
-        // Recibe el ID del producto desde el shortcode
-        $atts = shortcode_atts(array(
-            'product_id' => '',
-        ), $atts);
+function show_price_dsac($atts) {
+    date_default_timezone_set('America/Guayaquil');
 
-        // Encolar estilos y scripts
-        wp_enqueue_style('price-dsac-styles');
+    // Validar y sanitizar atributos
+    $atts = shortcode_atts(array(
+        'product_id' => '',
+    ), $atts);
 
-        // Obtiene el precio del producto
-        $product = wc_get_product($atts['product_id']);
-        
-        if ($product) {
-            $product_price = $product->get_regular_price();
-			
-			if($product->get_id() == 12209){
-				$product->set_sale_price(190);
-				$product->save();
-			}
-			
-            $product_sale_price = $product->get_sale_price();	
-            
-            $fecha_actual = new DateTime();
-            $dia = $fecha_actual->format('w');
-            
-            if ($dia < 20) {
-                if ($product_sale_price) {
-                    return '
-                    <div class="container-price">
-                        <h3 class="real-price-marca">De <span style="text-decoration: line-through;">$' . $product_price . '</span> a</h3>
-                        <p class="discount-price-marca">$' . $product_sale_price . '</p>
-                    </div>';
-                } else {
-                    return '<div class="container-price">
-                            <p class="discount-price-marca">USD$' . $product_price . '</p>
-                        </div>';
-                }
-            } else {
-                $product->set_sale_price($product_price);
-                $product->save();
-                return '<div class="container-price">
-                            <p class="discount-price-marca">USD$' . $product_price . '</p>
-                        </div>';
-            }
-        } else {
-            return '<div class="container-price">
-                    <p class="discount-price">Precio no disponible</p>
-                </div>';
-        }
+    $product_id = intval($atts['product_id']);
+    if (!$product_id) {
+        return '<div class="container-price"><p>Producto no válido.</p></div>';
     }
 
-    // Registrar el shortcode
-    add_shortcode('price_dsac', 'show_price_dsac');
+    // Encolar estilos
+    wp_enqueue_style('price-dsac-styles');
+
+    // Obtiene el producto
+    $product = wc_get_product($product_id);
+
+    if ($product && $product instanceof WC_Product) {
+        $product_price = $product->get_regular_price();
+
+        if ($product->get_id() == 12209) {
+            $product->set_sale_price(190);
+            $product->save();
+        }
+
+        $product_sale_price = $product->get_sale_price();
+
+        $fecha_actual = new DateTime('now', new DateTimeZone('America/Guayaquil'));
+        $dia = $fecha_actual->format('w');
+
+        if ($dia > 0) {
+            if ($product_sale_price) {
+                return '
+                <div class="container-price">
+                    <h3 class="real-price-marca">De <span style="text-decoration: line-through;">$' . esc_html($product_price) . '</span> a</h3>
+                    <p class="discount-price-marca">$' . esc_html($product_sale_price) . '</p>
+                </div>';
+            } else {
+                return '<div class="container-price">
+                        <p class="discount-price-marca">USD$' . esc_html($product_price) . '</p>
+                    </div>';
+            }
+        } else {
+            $product->set_sale_price($product_price);
+            $product->save();
+            return '<div class="container-price">
+                        <p class="discount-price-marca">USD$' . esc_html($product_price) . '</p>
+                    </div>';
+        }
+    } else {
+        return '<div class="container-price">
+                <p class="discount-price">Precio no disponible</p>
+            </div>';
+    }
 }
 
-// Hook para inicializar el plugin
-add_action('init', 'price_dsac_shortcode');
+// Registrar el shortcode
+add_shortcode('price_dsac', 'show_price_dsac');
