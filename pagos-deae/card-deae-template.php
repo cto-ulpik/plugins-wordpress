1edea6c6aa68cf9bdef938f53c99fa3f235511e2
CTO Ulpik <davichitoc@gmail.com>
2025-02-08 08:03:21 -0500

    fix: Agrego acciones posteriores

--- pagos-deae/card-deae-template.php
+++ pagos-deae/card-deae-template.php
@@ -29,5 +29,29 @@ $url = "https://eu-test.oppwa.com/v1/"; // URL del entorno de pruebas
     <!-- Formulario de pago -->
     <form action="<?php echo $baseUrl; ?>" class="paymentWidgets" data-brands="VISA MASTER DINERS DISCOVER AMEX">
     </form>
+
+
+    <script>
+        window.addEventListener("message", function(event) {
+            console.log("Evento recibido:", event);
+
+            if (event.origin.includes("oppwa.com")) {
+                console.log("Contenido del event.data:", event.data);
+
+                try {
+                    let paymentResponse = JSON.parse(event.data);
+                    console.log("Respuesta de Pago:", paymentResponse);
+
+                    if (paymentResponse.result && paymentResponse.result.code === "000.100.110") {
+                        document.getElementById("payment-status").innerHTML = "<h2>Pago Exitoso ✅</h2>";
+                    } else {
+                        document.getElementById("payment-status").innerHTML = "<h2>Error en el Pago ❌</h2>";
+                    }
+                } catch (error) {
+                    console.error("Error al procesar la respuesta del pago:", error);
+                }
+            }
+        }, false);
+    </script>
 </body>
 </html>
