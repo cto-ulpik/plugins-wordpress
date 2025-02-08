f92b9081af0867343c4df25476ba1e05fa98a4ba
CTO Ulpik <davichitoc@gmail.com>
2025-02-08 07:26:19 -0500

    fix: Agrego acciones posteriores

--- pagos-deae/pagos-deae.php
+++ pagos-deae/pagos-deae.php
@@ -13,14 +13,14 @@ if (!defined('ABSPATH')) {
 // Registrar las páginas personalizadas
 function pagos_deae_register_pages() {
     add_rewrite_rule('^pagos-deae/?$', 'index.php?pagos_deae_page=1', 'top');
-    add_rewrite_rule('^card-deae/?$', 'index.php?pagos_deae_pay_page=1', 'top');
+    add_rewrite_rule('^card-deae/?$', 'index.php?card-deae_page=1', 'top');
 }
 add_action('init', 'pagos_deae_register_pages');
 
 // Añadir query vars para las páginas personalizadas
 function pagos_deae_add_query_vars($vars) {
     $vars[] = 'pagos_deae_page'; // Página /pagos-deae
-    $vars[] = 'pagos_deae_pay_page'; // Página /card-deae
+    $vars[] = 'card-deae_page'; // Página /card-deae
     $vars[] = 'checkoutId'; // Parámetro adicional para /card-deae
     return $vars;
 }
@@ -36,13 +36,13 @@ function pagos_deae_template_redirect() {
 add_action('template_redirect', 'pagos_deae_template_redirect');
 
 // Interceptar la carga de /card-deae
-function pagos_deae_pay_template_redirect() {
-    if (get_query_var('pagos_deae_pay_page') == 1) {
+function card-deae_template_redirect() {
+    if (get_query_var('card-deae_page') == 1) {
         include plugin_dir_path(__FILE__) . 'card-deae.php';
         exit;
     }
 }
-add_action('template_redirect', 'pagos_deae_pay_template_redirect');
+add_action('template_redirect', 'card-deae_template_redirect');
 
 // Activar permalinks al activar el plugin
 function pagos_deae_flush_rewrite_rules() {
