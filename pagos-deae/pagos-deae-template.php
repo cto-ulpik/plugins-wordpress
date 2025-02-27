e71151775ca954f9a4e460933411d79815c4b5ed
CTO Ulpik <davichitoc@gmail.com>
2025-02-26 19:18:07 -0500

    feat: agregamos

--- pagos-deae/pagos-deae-template.php
+++ pagos-deae/pagos-deae-template.php
@@ -8,19 +8,72 @@
 <body>
     <h1>Realizar Pago</h1>
     <form method="POST">
-        <label for="amount">Monto:</label>
-        <input type="text" id="amount" name="amount" required>
+
+        <label for="firstName">Primer Nombre</label>
+        <input type="text" id="firsName" name="firstName" required>">
+        
+        <label for="secondName">Segundo Nombre</label>
+        <input type="text" id="secondName" name="secondName" required>">
+
+        <label for="lastName">Apellido</label>
+        <input type="text" id="lastName" name="lastName" required>">
+
+        
+        --
+        
+        <label for="email">Correo Electrónico:</label>
+        <input type="email" id="email" name="email" required>
+        --
+        
+        <label for="cedula">Cédula:</label>
+        <input type="text" id="cedula" name="cedula" required>">
+
+        <label for="telefono">Teléfono:</label>
+        <input type="text" id="telefono" name="telefono" required>">
+
+        <label for="direccion_cliente">Dirección:</label>
+        <input type="text" id="direccion_cliente" name="direccion_cliente" required>">
+
+
         <br>
         <button type="submit">Pagar</button>
     </form>
     
 
     <?php
+
+    
+    function generarIdentificador($longitud = 16) {
+        $longitud = rand(1, 16); // Define una longitud aleatoria entre 1 y 16
+        $caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $identificador = '';
+        
+        for ($i = 0; $i < $longitud; $i++) {
+            $identificador .= $caracteres[rand(0, strlen($caracteres) - 1)];
+        }
+        
+        return $identificador;
+    }
+    
+    // Ejemplo de uso
+    echo generarIdentificador();
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $amount = sanitize_text_field($_POST['amount']);
+        $firstName = sanitize_text_field($_POST['firstName']);
+        $secondName = sanitize_text_field($_POST['secondName']);
+        $lastName = sanitize_text_field($_POST['lastName']);
+        $email = sanitize_text_field($_POST['email']);
 
-        function request($amount) {
+        $cedula = sanitize_text_field($_POST['cedula']);
+        $telefono = sanitize_text_field($_POST['telefono']);
 
+        $direccion_cliente = sanitize_text_field($_POST['direccion_cliente']);
+
+
+        function request($firstName, $secondName, $lastName, $email, $cedula, $telefono, $direccion_cliente) {
+            $amount = 29;
+            
+            
             // Calcular los impuestos correctamente
             $baseImponible = round($amount / 1.15, 2); // Base sin IVA (asumiendo 12% de IVA)
             $iva = round($amount - $baseImponible, 2); // IVA calculado
@@ -31,16 +84,32 @@
 
             $url = "https://eu-test.oppwa.com/v1/checkouts";
             $data = "entityId=8ac7a4c994bb78290194bd40497301d5" .
-                    "&amount=" . $amount .
+                    "&amount =" . $amount .
                     "&currency=USD" .
                     "&paymentType=DB" .
+
+                    "&customer.givenName=" . $firstName .
+                    "&customer.middleName=" . $secondName .
+                    "&customer.surname=" . $lastName .
 
                     "&customer.givenName=Nestor" .
                     "&customer.middleName=David" .
                     "&customer.surname=Castillo" .
-
+                    
                     "&customer.ip=" . $_SERVER['REMOTE_ADDR'] .
-                    "&customer.email=" .
+
+                    "&customer.email=" . $email .
+                    "&customer.identificationDocType=IDCARD".
+                    "&customer.identificationDocId=".$cedula.
+
+                    "&customer.phone=".$telefono.
+                    
+                    "&billing.street=".$direcion_cliente.
+                    "&billing.country=EC".
+
+                    "&shipping.street=".$direccion_cliente.
+                    "&shipping.country=EC".
+                    
 
                     "&customParameters[SHOPPER_ECI]=0103910" .
                     "&customParameters[SHOPPER_PSERV]=17913101" .
@@ -53,7 +122,14 @@
 
                     "&risk.parameters[USER_DATA2]=DATAFAST" .
                     "&customParameters[SHOPPER_VERSIONDF]=2" .
-                    "&testMode=EXTERNAL";
+                    "&testMode=EXTERNAL".
+                    
+                    
+                    "&cart.items[0].name=DEAESUSCRIPCION".
+                    "&cart.items[0].description=suscripcion".
+                    "&cart.items[0].price=29".
+                    "&cart.items[0].quantity=1".
+                    ;
 
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
