748c8945e08924594b0b16e2d2db3eabb3e4da8b
CTO Ulpik <davichitoc@gmail.com>
2025-02-26 21:06:29 -0500

    feat: agregamos

--- pagos-deae/card-deae-template.php
+++ pagos-deae/card-deae-template.php
@@ -21,16 +21,6 @@ $url = "https://eu-test.oppwa.com/v1/"; // URL del entorno de pruebas
     <title>Pagar con Tarjeta</title>
     <!-- Incluir el script de pago con el checkoutId -->
     <script src="<?php echo $url; ?>paymentWidgets.js?checkoutId=<?php echo $checkoutId; ?>"></script>
-    
-</head>
-<body>
-    <h1>Formulario de Pago</h1>
-    <p>Completa la información de tu tarjeta para proceder con el pago.</p>
-    
-    <!-- Formulario de pago -->
-    <form action="<?php echo $baseUrl; ?>" class="paymentWidgets" data-brands="VISA MASTER DINERS DISCOVER AMEX">
-    </form>
-
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script type="text/javascript">
     var wpwlOptions = {
@@ -50,6 +40,16 @@ $url = "https://eu-test.oppwa.com/v1/"; // URL del entorno de pruebas
         }
     }
     </script>
+</head>
+<body>
+    <h1>Formulario de Pago</h1>
+    <p>Completa la información de tu tarjeta para proceder con el pago.</p>
+    
+    <!-- Formulario de pago -->
+    <form action="<?php echo $baseUrl; ?>" class="paymentWidgets" data-brands="VISA MASTER DINERS DISCOVER AMEX">
+    </form>
+
+    
     
     <script>
         window.addEventListener("message", function(event) {
