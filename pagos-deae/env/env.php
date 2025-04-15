6e0a8df3e33c7e9f6a544a4e636668eb9d93c8e2
CTO Ulpik <davichitoc@gmail.com>
2025-04-15 17:51:47 -0500

    fix: sin correos

--- pagos-deae/env/env.php
+++ pagos-deae/env/env.php
@@ -3,22 +3,22 @@
 
 /// ACCESOS DE PRUEBA
 
-// $id_entidad_datafast = "8ac7a4c994bb78290194bd40497301d5";
-// $access_token_datafast = "OGE4Mjk0MTg1YTY1YmY1ZTAxNWE2YzhjNzI4YzBkOTV8YmZxR3F3UTMyWA==";
+$id_entidad_datafast = "8ac7a4c994bb78290194bd40497301d5";
+$access_token_datafast = "OGE4Mjk0MTg1YTY1YmY1ZTAxNWE2YzhjNzI4YzBkOTV8YmZxR3F3UTMyWA==";
 
-// $mid_datafast = "1000000406";
-// $tid_datafast = "PD100406";
-// $serv_datafast = "17913101";
+$mid_datafast = "1000000505";
+$tid_datafast = "PD100406";
+$serv_datafast = "17913101";
 
-// $url_datafast = "https://eu-test.oppwa.com";
+$url_datafast = "https://eu-test.oppwa.com";
 
 /// ACCESOS DE PRODUCCION
 
-$id_entidad_datafast = "8acda4cc95f5c7b70196112c671c0531";
-$access_token_datafast = "OGFjOWE0Y2E4YWIxZjZlMzAxOGFjY2E2MTgzYzcwOTZ8NzlUTkpkd0ZqZA==";
+// $id_entidad_datafast = "8acda4cc95f5c7b70196112c671c0531";
+// $access_token_datafast = "OGFjOWE0Y2E4YWIxZjZlMzAxOGFjY2E2MTgzYzcwOTZ8NzlUTkpkd0ZqZA==";
 
-$mid_datafast = "4300000804";
-$tid_datafast = "BP456083";
-$serv_datafast = "17913101";
+// $mid_datafast = "4300000804";
+// $tid_datafast = "BP456083";
+// $serv_datafast = "17913101";
 
-$url_datafast = "https://eu-prod.oppwa.com";
+// $url_datafast = "https://eu-prod.oppwa.com";
