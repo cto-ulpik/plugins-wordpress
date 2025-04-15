3822a66f410726268455dfb54bab1bf387e9fe32
CTO Ulpik <davichitoc@gmail.com>
2025-04-15 17:27:15 -0500

    fix: sin correos

--- pagos-deae/finalizar-deae-template.php
+++ pagos-deae/finalizar-deae-template.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once plugin_dir_path(__FILE__) . 'emails/sendEmail.php';
+// require_once plugin_dir_path(__FILE__) . 'emails/sendEmail.php';
 
 // Verificar si el parámetro ID está presente
 if (!isset($_GET['id'])) {
